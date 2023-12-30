10ba28f78d2d77a8ef583b6cfcc5c04a156c6b82
Stefano Alberto Russo <stefano.russo@gmail.com>
2023-12-30 16:44:54 +0100

    Added regression test script for testing with Docker from Python 3.6 to 3.12.

---
+++ regression_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+for PYTHON_VERSION in "3.6" "3.7"  "3.8"  "3.9"  "3.10"  "3.11"  "3.12"; do
+
+    echo ""
+    echo "=========================="
+    echo " Testing with Python $PYTHON_VERSION "
+    echo "=========================="
+
+    docker run -it -v $PWD:/Propertime python:$PYTHON_VERSION /bin/bash -c "cd /Propertime && pip install -r requirements.txt && python -m unittest discover"
+done
