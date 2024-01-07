4fd313d325bed5174052b7dcaa00dab6178c3525
Stefano Alberto Russo <stefano.russo@gmail.com>
2024-01-07 17:44:49 +0100

    Minor fixes in the PyPI helper script.

--- pypi.sh
+++ pypi.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Ensure pip install twine wheel
+# Tested with:
+# pip install twine==4.0.2 wheel==0.42.0
+#Ensure pip install twine wheel
 
 if [[ "x$1" == "xbuild" ]] ; then
     python3 setup.py sdist bdist_wheel
@@ -9,8 +11,10 @@ elif [[ "x$1" == "xtestpush" ]] ; then
     twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 
 elif [[ "x$1" == "xpush" ]] ; then
-    echo "Will push to REAL PyPI!!! Sleeping 60 secs"
-    sleep 60
+    echo ""
+    echo " !!! Will push to REAL PyPI !!! "
+    echo ""
+    sleep 3
     twine upload dist/*
 
 elif [[ "x$1" == "xclean" ]] ; then
