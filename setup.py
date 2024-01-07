2226a716f41676cde7b804ef3904695ceb453d8a
Stefano Alberto Russo <stefano.russo@gmail.com>
2024-01-07 17:46:55 +0100

    Bump version to v0.2.1.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='propertime',
-      version='0.2.0',
+      version='0.2.1',
       description='An attempt at proper time management in Python.',
       long_description="""
 Propertime is an attempt at implementing proper time management in Python,
